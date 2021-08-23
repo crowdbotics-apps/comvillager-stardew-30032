1c9019707d377fff5e39561f2f766215af4a815b
Crowdbotics <team@crowdbotics.com>
2021-08-23 04:33:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "comvillager_stardew_30032Identifier",
+  url: "https://comvillager_stardew_30032Identifier.botics.co",
+  api: "https://comvillager_stardew_30032Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
